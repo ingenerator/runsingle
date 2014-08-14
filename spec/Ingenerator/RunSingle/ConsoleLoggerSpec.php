2a443b811e2f374099b5721dff0f6c61c56d1977
matthias <matthias@ingenerator.com>
2014-08-14 16:44:34 +0100

    Implement ConsoleLogger.
        Add required psr\log to composer.

---
+++ spec/Ingenerator/RunSingle/ConsoleLoggerSpec.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * Console logger spec
+ *
+ * @author    Matthias Gisder <matthias@ingenerator.com>
+ * @copyright 2014 inGenerator Ltd
+ * @licence   BSD
+ */
+
+
+namespace spec\Ingenerator\RunSingle;
+
+use Psr\Log\LogLevel;
+use spec\ObjectBehavior;
+use Prophecy\Argument;
+
+class ConsoleLoggerSpec extends ObjectBehavior
+{
+    /**
+     * Use $this->subject to get proper type hinting for the subject class
+     * @var \Ingenerator\RunSingle\ConsoleLogger
+     */
+    protected $subject;
+
+    function it_is_initializable()
+    {
+        $this->subject->shouldHaveType('Ingenerator\RunSingle\ConsoleLogger');
+    }
+
+    function it_prepends_iso8601_timestamp_to_log()
+    {
+        $content = $this->given_logged(LogLevel::INFO, 'test');
+        list ($timestamp, $message) = explode(': ', $content);
+        $timestamp = \DateTime::createFromFormat('Y-m-d\TH:i:se', $timestamp);
+        expect($timestamp)->toBeAnInstanceOf('\DateTime');
+        expect((time() - $timestamp->getTimestamp()) <= 1)->toBe(TRUE);
+    }
+
+    function it_prepends_log_level_to_log()
+    {
+        $content = $this->given_logged(LogLevel::INFO, 'test');
+        expect($content)->toMatch('/^[^ ]+ \[info\] /');
+
+    }
+
+    function it_logs_message_after_timestamp_and_loglevel()
+    {
+        $content = $this->given_logged(LogLevel::INFO, 'test');
+        expect($content)->toMatch('/^[^ ]+ [^ ]+ test\n$/');
+    }
+
+    function given_logged($level, $message, $context = array())
+    {
+        ob_start();
+        $this->subject->log($level, $message, $context);
+        return ob_get_clean();
+    }
+
+}
---
+++ src/Ingenerator/RunSingle/ConsoleLogger.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Ingenerator\RunSingle;
+
+class ConsoleLogger extends \Psr\Log\AbstractLogger
+{
+    const DATE_FORMAT_INTL = 'Y-m-d\TH:i:se';
+
+     /**
+     * Logs with an arbitrary level.
+     *
+     * @param mixed $level
+     * @param string $message
+     * @param array $context
+     * @return null
+     */
+    public function log($level, $message, array $context = array())
+    {
+        $time = new \DateTime;
+        echo sprintf(
+            '%s: [%s] %s'.\PHP_EOL,
+            $time->format('c'),
+            $level,
+            $message
+        );
+    }
+
+}
